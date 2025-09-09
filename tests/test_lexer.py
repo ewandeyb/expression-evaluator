2e511ac2c5fbbde3c74747eafea3930c58e053d3
Aaron Dave Siapuatco <aaronsiapuatco@gmail.com>
2025-09-09 15:45:00 +0800

    test: add lexer tests

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,5 +1,29 @@
 import pytest
+from lexer import Lexer, Token, TokenType
 
-
-def test_placeholder():
-  assert True
+@pytest.mark.parametrize("input_string, expected_tokens", [
+    ("1 + 2", [
+        Token(TokenType.INTEGER, "1"),
+        Token(TokenType.PRED1, "+"),
+        Token(TokenType.INTEGER, "2"),
+        Token(TokenType.EOF, "")
+    ]),
+    ("var_name", [
+        Token(TokenType.VAR, "var_name"),
+        Token(TokenType.EOF, "")
+    ]),
+    ("3 * (4 - 5)", [
+        Token(TokenType.INTEGER, "3"),
+        Token(TokenType.PRED2, "*"),
+        Token(TokenType.PRED3, "("),
+        Token(TokenType.INTEGER, "4"),
+        Token(TokenType.PRED1, "-"),
+        Token(TokenType.INTEGER, "5"),
+        Token(TokenType.PRED3, ")"),
+        Token(TokenType.EOF, "")
+    ])
+])
+def test_lexer(input_string, expected_tokens):
+    lexer = Lexer(input_string)
+    tokens = lexer.tokenize()
+    assert tokens == expected_tokens
