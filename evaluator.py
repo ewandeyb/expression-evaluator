18e8063a2f682a6b486398513b5f8e820690ee8d
44mira <legolaslada@gmail.com>
2025-09-21 04:16:32 +0800

    feat: add evaluator methods

--- evaluator.py
+++ evaluator.py
@@ -19,7 +19,7 @@ class Evaluator:
 
     def __init__(self, ast, symbol_table) -> None:
         self.ast = ast
-        self.stack = []
+        self.postfix = []
         self.symbol_table = symbol_table
 
     def _walk(self, node):
@@ -27,23 +27,23 @@ class Evaluator:
 
         if node_type == NodeType.ASSIGNMENT:
             _, identifier, expr = node
-            self.stack.append(identifier)
+            self.postfix.append(identifier)
             self._walk(expr)
-            self.stack.append("=")
+            self.postfix.append("=")
 
         elif node_type == NodeType.BINARY_OP:
             _, operator, left, right = node
             self._walk(left)
             self._walk(right)
-            self.stack.append(operator)
+            self.postfix.append(operator)
 
         elif node_type == NodeType.NUMBER:
             _, value = node
-            self.stack.append(value)
+            self.postfix.append(value)
 
         elif node_type == NodeType.VARIABLE:
             _, name = node
-            self.stack.append(name)
+            self.postfix.append(name)
 
         else:
             raise ValueError(f"Unknown AST node: {node_type}")
@@ -52,7 +52,29 @@ class Evaluator:
         self._walk(self.ast)
 
     def execute(self):
-        pass
+        var_name = self.postfix[0]
+
+        stack = []
+
+        # ignore first and last symbol
+        for i in range(1, len(self.postfix) - 1):
+            node = self.postfix[i]
+
+            if type(node) is int:
+                stack.append(node)
+
+            elif node not in "+/*%-":
+                if node not in self.symbol_table:
+                    raise NameError(f"Variable `{node}` is not defined.")
+
+                stack.append(self.symbol_table[node])
+
+            else:
+                b, a = stack.pop(), stack.pop()
+                stack.append(eval(f"{a}{node}{b}"))
+
+        self.symbol_table[var_name] = stack[0]
+        return var_name
 
     def __str__(self) -> str:
-        pass
+        return " ".join(str(e) for e in self.postfix)
---
+++ tests/test_evaluator.py
@@ -0,0 +1,69 @@
+from parser import Parser
+
+import pytest
+
+from evaluator import Evaluator
+from lexer import Lexer
+
+
+def _eval(string: str, symbol_table: dict):
+    lexer = Lexer(string)
+    parser = Parser(lexer.tokenize())
+    ast = parser.parse()
+    evaluator = Evaluator(ast, symbol_table)
+    evaluator.evaluate()
+
+    return evaluator
+
+
+@pytest.mark.parametrize(
+    "input_str, symbol_table, expected_str,"
+    "expected_symbol_table, expected_identifier",
+    [
+        pytest.param(
+            "a=1",
+            {},
+            "a 1 =",
+            {"a": 1},
+            "a",
+            id="simple",
+        ),
+        pytest.param(
+            "a=4+7*3",
+            {},
+            "a 4 7 3 * + =",
+            {"a": 25},
+            "a",
+            id="complex",
+        ),
+        pytest.param(
+            "a=b+5",
+            {"b": 3},
+            "a b 5 + =",
+            {"a": 8, "b": 3},
+            "a",
+            id="symbol_table",
+        ),
+    ],
+)
+def test_evaluator(
+    input_str,
+    symbol_table,
+    expected_str,
+    expected_symbol_table,
+    expected_identifier,
+):
+    evaluator = _eval(input_str, symbol_table)
+
+    assert str(evaluator) == expected_str
+
+    identifier = evaluator.execute()
+    assert symbol_table == expected_symbol_table
+    assert identifier == expected_identifier
+
+
+def test_undefined_reference():
+    evaluator = _eval("a=b+3", {})
+
+    with pytest.raises(NameError):
+        evaluator.execute()
